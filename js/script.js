356b0162c44a5113425a44ba6c2930f798314ad3
tiagoalmeidacorreia <tiagoalmeida.correia@gmail.com>
2018-01-10 11:25:58 +0000

    Form/smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel();
+
+    $('a[href*=\\#]').click(function(event) {
+        $('html, body').animate(
+            {
+                scrollTop: $($.attr(this, 'href')).offset().top
+            },
+            500
+        );
+        event.preventDefault();
+    });
 });
