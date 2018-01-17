31ec0041e9c36d5c42369cf24125b71e9a969518
Andres <andresglez5@gmail.com>
2018-01-17 18:25:08 +0100

    Write some utils for manual form submit

--- js/script.js
+++ js/script.js
@@ -11,3 +11,39 @@ $(document).ready(function() {
         event.preventDefault();
     });
 });
+
+//Form (AJAX)
+function GETreq(url,callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              callback(this.responseText);
+        }
+      };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function POSTreq(url,body,callback) {
+    var objJSON = JSON.stringify(obj);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText)
+        }
+      };
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(objJSON);
+}
+
+/*
+//Validate (already donde with requires)
+function validate() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    //...
+    
+}
+*/
+
